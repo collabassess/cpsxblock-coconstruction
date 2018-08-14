da7edfb3d948c64153a5ed6d43224f2d545b1d72
Patrick Anker <patricksanker@gmail.com>
2018-08-14 12:09:42 -0400

    First draft

--- constructcpsxblock/constructcpsxblock/constructcpsxblock.py
+++ constructcpsxblock/constructcpsxblock/constructcpsxblock.py
@@ -1,24 +1,21 @@
-"""TO-DO: Write a description of what this XBlock is."""
+import re
+import json
 
 import pkg_resources
 from xblock.core import XBlock
 from xblock.fields import Integer, Scope, String
 from xblock.fragment import Fragment
 
+import requests
 
+BASE_URL = "http://ec2-54-156-197-224.compute-1.amazonaws.com"
+PORT     = "3050"
+
+@XBlock.needs('user')
 class CoConstructCPSXBlock(XBlock):
     """
-    TO-DO: document what your XBlock does.
+    Add-on to the CPSXBlock which allows for co-constructed problems (those which require previous information for solutions)
     """
-
-    # Fields are defined on the class.  You can access them in your code as
-    # self.<fieldname>.
-
-    # TO-DO: delete count, and define your own fields.
-    count = Integer(
-        default=0, scope=Scope.user_state,
-        help="A simple counter, to show something happening",
-    )
 
     provider_userA = String(
         default="", scope=Scope.settings,
@@ -30,7 +27,12 @@ class CoConstructCPSXBlock(XBlock):
         help="The target module for Version 2's \"Part 1\""
     )
 
-    receiver = String(
+    receiver_userA = String(
+        default="", scope=Scope.settings,
+        help="The target module for the \"Part 2\" that requires information from both Versions' Part 1s"
+    )
+
+    receiver_userB = String(
         default="", scope=Scope.settings,
         help="The target module for the \"Part 2\" that requires information from both Versions' Part 1s"
     )
@@ -48,23 +50,123 @@ class CoConstructCPSXBlock(XBlock):
         """
         html = self.resource_string("static/html/constructcpsxblock.html")
         frag = Fragment(html.format(self=self))
-        frag.add_css(self.resource_string("static/css/constructcpsxblock.css"))
+        # frag.add_css(selaf.resource_string("static/css/constructcpsxblock.css"))
         frag.add_javascript(self.resource_string("static/js/src/constructcpsxblock.js"))
-        frag.initialize_js('CoConstructCPSXBlock')
+
+        # Actual init
+        initdict = {
+            'providerA': self.provider_userA, 
+            'providerB': self.provider_userB, 
+            'receiverA': self.receiver_userA,
+            'receiverB': self.receiver_userB
+        }
+
+        frag.initialize_js('CoConstructCPSXBlock', initdict)
         return frag
-
-    # TO-DO: change this handler to perform your own actions.  You may need more
-    # than one handler, or you may not need any handlers at all.
+    
+    @property
+    def course_id(self):
+        if hasattr(self, 'xmodule_runtime'):
+            if hasattr(self.xmodule_runtime.course_id, 'to_deprecated_string'):
+                return self.xmodule_runtime.course_id.to_deprecated_string()
+            else:
+                return self.xmodule_runtime.course_id
+        
+        return 'course-v1:NYU+DEMO_101+2018_T1'
+    
+    @staticmethod
+    def short_module_id(module):
+        return re.match(r"problem\+block@(\w+)", module)
+    
     @XBlock.json_handler
-    def increment_count(self, data, suffix=''):
+    def problem_submit(self, data):
         """
-        An example handler, which increments the data.
+        Sends the server a notice that a receiver has been submitted
         """
-        # Just to show data coming in...
-        assert data['hello'] == 'world'
 
-        self.count += 1
-        return {"count": self.count}
+        rec = data['receiver']
+        assert (rec == self.receiver_userA) or (rec == self.receiver_userB) # Just in case
+
+        uid = self.get_userid()
+
+        if isinstance(uid, str):
+            raise Exception("User not found")
+        
+        ans       = data['answer']
+        total_ans = ""
+
+        # Collect provided answers
+        if rec == self.receiver_userA:
+            user_pans    = self.get_user_provider_answer(uid, self.provider_userA)
+            partner_pans = self.get_partner_provider_answer(uid, self.provider_userB)
+
+            total_ans = self.format_total_answer(user_pans, partner_pans, ans)
+        else:
+            user_pans    = self.get_user_provider_answer(uid, self.provider_userB)
+            partner_pans = self.get_partner_provider_answer(uid, self.provider_userA)
+
+            total_ans = self.format_total_answer(partner_pans, user_pans, ans)
+        
+        # Send grader collected answers
+        res = self.post_edx(rec, total_ans)
+
+        return res.text
+    
+    def post_api(self, uri, json_data):
+        return requests.post("{0}:{1}{2}".format(BASE_URL, PORT, uri), json=json_data)
+    
+    def post_edx(self, problem_module, data_string):
+        url = "{0}/courses/{1}/xblock/{2}/handler/xmodule_handler/problem_check".format(BASE_URL, self.course_id, problem_module)
+        key = "input_{}_2_1".format(CoConstructCPSXBlock.short_module_id(problem_module))
+
+        return requests.post(url, json={key: data_string})
+    
+    def format_total_answer(self, provider_answer_A, provider_answer_B, receiver_ans):
+        return json.dumps({"provider_A": provider_answer_A, "provider_B": provider_answer_B, "answer": receiver_ans})
+        
+    def get_partner_provider_answer(self, current_user, provider_module):
+        """
+        Fetches the answer for the partner's provider problem
+        """
+        data = {"curr_user": current_user, "problem_id": provider_module}
+
+        res    = self.post_api("/sessions/getPartnerAnswerForProblem", data)
+        resobj = json.loads(res.text)
+
+        if "ans" not in resobj:
+            raise Exception(resobj["err"])
+        else:
+            return resobj["ans"]
+    
+    def get_user_provider_answer(self, current_user, provider_module):
+        """
+        Fetches the answer for the user's provider problem
+        """
+        data = {"curr_user": current_user, "problem_id": provider_module}
+
+        res    = self.post_api("/sessions/getUserAnswerForProblem", data)
+        resobj = json.loads(res.text)
+
+        if "ans" not in resobj:
+            raise Exception(resobj["err"])
+        else:
+            return resobj["ans"]
+    
+    def get_user(self):
+        """
+        Get an attribute of the current user.
+        """
+        user_service = self.runtime.service(self, 'user')
+        if user_service:
+            # May be None when creating bok choy test fixtures
+            return user_service.get_current_user()
+        return None
+
+    def get_userid(self):
+        try:
+            return self.get_user().opt_attrs['edx-platform.user_id']
+        except Exception:
+            return '4'
 
     # TO-DO: change this to create the scenarios you'd like to see in the
     # workbench while developing your XBlock.
--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Indicator from "./indicator/Indicator";
+// import React, { Component } from "react";
+// import ReactDOM from "react-dom";
+// import Indicator from "./indicator/Indicator";
+
+function shortModuleID(moduleID) {
+    let blockPattern = /problem\+block@(\w+)/g,
+        shortID      = "";
+    
+    let match = blockPattern.exec(moduleID);
+
+    if (match != null) {
+        shortID = match[1];
+    }
+
+    return shortID;
+}
+
+// Module IDs for the co-constructed vertical subset
+const providerA = data.providerA,
+      providerB = data.providerB,
+      receiverA = data.receiverA,
+      receiverB = data.receiverB;
 
 function updateCount(result) {
     $('.count', element).text(result.count);
@@ -17,7 +36,24 @@ $('p', element).click(function(eventObject) {
     });
 });
 
-$(function ($) {
-    alert('hello');
-    ReactDOM.render(<Indicator />, document.getElementById("component-test"))
+$(`#problem_${shortModuleID(receiverA)} .problem .action button`).click(e => {
+    let handle = runtime.handlerUrl(element, 'problem_submit'),
+        ans    = $(`#input${shortModuleID(receiverB)}_2_1`)[0].value;
+
+    $.ajax({
+        type: "POST",
+        url: handle,
+        data: JSON.stringify({receiver: receiverA, answer: ans})
+    });
+});
+
+$(`#problem_${shortModuleID(receiverB)} .problem .action button`).click(e => {
+    let handle = runtime.handlerUrl(element, 'problem_submit'),
+        ans    = $(`#input${shortModuleID(receiverB)}_2_1`)[0].value;
+
+    $.ajax({
+        type: "POST",
+        url: handle,
+        data: JSON.stringify({receiver: receiverB, answer: ans})
+    });
 });
