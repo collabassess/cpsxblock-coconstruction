e8901860d027a07362df789040823aef7d1ed6d5
Patrick Anker <patricksanker@gmail.com>
2018-08-16 14:23:44 -0400

    Wrong shell command

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo rm -rf constructcpsxblock
 
 # Move things into place and install
 cd -
-sudo mv constructcpsxblock /edx/app/edxapp/constructcpsxblock
+sudo cp -R constructcpsxblock /edx/app/edxapp/constructcpsxblock
 
 cd /edx/app/edxapp
 sudo -u edxapp /edx/bin/pip.edxapp install constructcpsxblock/ --upgrade --no-deps
